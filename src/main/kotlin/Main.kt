be99e838ff0106ec1fc266e78763c61a80d029ce
LumakanA <lumakan@yandex.com>
2022-11-26 17:51:12 +0300

    init branch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Hello World!")
-    println("Hello World!")
-    println("Hello World!")
+    println("Разработать программу, которая для заданной строки s подсчитывает количество букв в строке")
+    print("Введите текст строки - ")
+    val stringVvod: String = (readln())
+    val itog = stringVvod.replace("[^a-zA-Zа-яёА-ЯЁ]".toRegex(), "")
+    print("Число букв в строке = ")
+    println(itog.count())
 }
