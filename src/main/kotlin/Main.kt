74bd421161be34f0cdf41ab9fe16579ce259cd8e
LumakanA <lumakan@yandex.com>
2022-12-28 02:25:50 +0300

    Задание 1 с классами, вариант 14

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Hello World!")
-    println("Hello World!")
-    println("Hello World!")
+//класс товара
+open class Product {
+    // Цена товара
+    open val price: Int = 0
+}
+
+//класс часов, унаследованный от класса Product
+class Watch : Product() {
+    // Переопределенное свойство цены товара
+    override val price: Int = 100
+}
+
+//класс кондитерских изделий, унаследованный от класса Product
+class Pastry : Product() {
+    //новое свойство цены товара
+    override val price: Int = 50
+}
+
+fun main() {
+//класс часов
+    val watch = Watch()
+
+//класс кондитерских изделий
+    val pastry = Pastry()
+
+//выводим на экран самый дешевый товар
+    if (watch.price < pastry.price) {
+        println("Цена продукта: ${watch.price}")
+    } else {
+        println("Цена продукта: ${pastry.price}")
+    }
+
 }
